b213b1b8c473dfb043e8f80daa71a2c0417400dc
ddsfda99 <168000702+ddsfda99@users.noreply.github.com>
2025-06-19 13:53:17 +0800

    Create train_lora_monet.sh

---
+++ scripts/monet/train_lora_monet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+accelerate launch training/train_dreambooth_lora.py \
+  --pretrained_model_name_or_path /home/jenny/fine_tuning/models/stable-diffusion-v1-5 \
+  --instance_data_dir /home/jenny/fine_tuning/trainsets/monet \
+  --output_dir /home/jenny/fine_tuning/outputs/lora_monet \
+  --instance_prompt "<monet-style>" \
+  --resolution 512 \
+  --train_batch_size 1 \
+  --max_train_steps 200 \
+  --mixed_precision fp16
